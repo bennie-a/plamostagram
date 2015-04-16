f096120e52b572f7d91db0365e04926bcdb7d352
紅山藍子 <saltory72@gmail.com>
2015-04-16 16:20:19 +0900

    Instagram API から画像データを取得する処理を追加。 Lazy Load Pluginによる画像読み込み遅延処理を追加。

--- js/instagram.js
+++ js/instagram.js
@@ -6,6 +6,12 @@ $(window).scroll(function() {
     } else {
       movePageTop.fadeOut('slow');
     }
+
+    var header = $('#header');
+    var list = $('#list');
+    if ((header.height() + list.height()) - now < 600) {
+        loadInstagram(list.data("json"));
+    }
 });
 
 $(function(){
@@ -14,7 +20,7 @@ $(function(){
   list.on('didLoadInstagram', didLoadInstagram);
   list.instagram({
     hash:'modelkit',
-    count: 50,
+    count: 1000,
     clientId: clientId
   });
 
@@ -23,9 +29,18 @@ $(function(){
   });
 });
 
+$(window).load(function() {
+  $("#list img").lazyload({
+    effect: 'fadeIn',
+        effectspeed: 1000
+  });
+});
+
 function createPhotoElement(photo) {
     var image = $('<img>')
-      .attr('src', photo.images.low_resolution.url)
+       // .attr('src', photo.images.low_resolution.url)
+       .attr('src', 'http://placehold.it/306x306/ff6347')
+      .attr('data-original', photo.images.low_resolution.url)
       .attr('alt', photo.caption.text);
 
     image = $('<a>')
@@ -46,4 +61,33 @@ function didLoadInstagram(event, response) {
   $.each(response.data, function(i, photo) {
     $('#list ul').append(createPhotoElement(photo));
   });
+}
+
+function loadInstagram(photoData) {
+  loadCount = 10;
+  maxCount = 20;
+  var startIndex = $('#list ul').children('li').length;
+  var $photoData;
+  if (startIndex < maxCount) {
+    $photoData = delimitData(startIndex, loadCount, photoData);
+    $.each($photoData, function(i, photo) {
+      $('#list ul').append(createPhotoElement(photo));
+    });
+  } else {
+    var overCount = (startIndex) - (maxCount);
+    var adjustCont = (startIndex)-(overCount);
+    $photoData = delimitData(startIndex, adjustCont, photoData);
+    $.each($photoData, function(i, photo) {
+      $('#list ul').append(createPhotoElement(photo));
+    });
+  }
+  if ($.isEmptyObject($("#list").data("json"))) {
+    $("#list").data("json", photoData);
+  }
+}
+
+function delimitData(startIndex, count, photoData) {
+    return $.grep(photoData, function(photo, index) {
+      return index >= startIndex && index < count;
+    });
 }
