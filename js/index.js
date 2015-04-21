a49b32b382d0490c7cc87b4391d4f10357ddc0ee
紅山藍子 <saltory72@gmail.com>
2015-04-21 12:09:29 +0900

    同じ写真を何回も表示する不具合を修正。

--- js/index.js
+++ js/index.js
@@ -8,17 +8,19 @@ $(window).scroll(function() {
     }
 });
 
-$(function(){
-		$("#more").on('inview', function(event, isInView, visiblePartX, visiblePartY) { 
-			if (visiblePartY == 'both') {
-				searchInstagram();
+  var clientId = 'b570746883c541058a09f7e21e96dff5';
+$(function() {
+	  var $more = $("#more");
+	  $more.on('didLoadInstagram', didLoadInstagram);
+	  $more.on('inview', function(event, isInView, visiblePartX, visiblePartY) { 
+			if (isInView && visiblePartY == 'both') {
+				   $("#more").instagram({
+					hash:'modelkit',
+					count: 30,
+					 url: next_url,         
+					clientId: clientId
+				  });
 			}
-			
-			var delaySpeed = 100;
-        		var fadeSpeed = 1000;
-        		$('ul li').each(function(i){
-            		$(this).delay(1000).css({opacity:'0'}).animate({opacity:'1'},fadeSpeed);
-	        });
 		});
 
 	$('#goToTop').click(function() {
@@ -28,23 +30,11 @@ $(function(){
 
 var next_url;
 
-function searchInstagram() {
-	  var clientId = 'b570746883c541058a09f7e21e96dff5';
-	  var list = $('#list');
-	  list.on('didLoadInstagram', didLoadInstagram);
-	  list.instagram({
-  		hash:'modelkit',
-  		count: 30,
-	     url: next_url,         
-  		clientId: clientId
-	  });
-}
-
 function createPhotoElement(photo) {
     var image = $('<img>')
        .attr('src', photo.images.low_resolution.url)
-      .attr('alt', photo.caption.text);
-
+      .attr('alt', photo.caption ? photo.caption.text : photo.tags.join(" "));
+	  
     image = $('<a>')
       .attr('target', '_blank')
       .attr('href', photo.link)
@@ -60,7 +50,6 @@ function didLoadInstagram(event, response) {
     return;
   }
 
-  console.log(response.pagination.next_url);
   $.each(response.data, function(i, photo) {
 	    $('#more').before(createPhotoElement(photo));
   });
